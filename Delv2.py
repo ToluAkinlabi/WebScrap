14445cfd23fa29c06cd5773802b811f22a46ecb8
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-06-03 02:20:25 -0400

    Updated Table Details

--- Delv2.py
+++ Delv2.py
@@ -9,3 +9,28 @@ parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
 #Baseline Table Scrap
 #Table header
 
+baselineHeading = parseClinicPage.find_all('span', class_="ct-header2")[1]
+strippedBaselineHeading = baselineHeading.text.strip()
+print(strippedBaselineHeading)
+
+#Table Title
+tableTitle = parseClinicPage.find("td", class_="de-baselineLabelCell", colspan="2")
+strippedtableTitle = tableTitle.text
+print(strippedtableTitle)
+
+standardSurgery = parseClinicPage.find("td", class_="de-baselineLabelCell", colspan="2").find_next()
+strippedStandardSurgery = standardSurgery.text
+print(strippedStandardSurgery)
+
+standardSurgeryWithOOC = parseClinicPage.find("td", class_="de-baselineLabelCell", colspan="2").find_next().find_next()
+strippedStandardSurgeryWithOOC = standardSurgeryWithOOC.text
+print(strippedStandardSurgeryWithOOC)
+
+total = parseClinicPage.find("td", class_="de-baselineLabelCell", colspan="2").find_next().find_next().find_next()
+strippedTotal = total.text
+print(strippedTotal)
+
+#Table Title Content
+tableRowDesc = parseClinicPage.find("span", class_="COLLAPSE de-showAndHide")
+strippedTableRowDesc = tableRowDesc.text.strip()
+print(strippedTableRowDesc)
