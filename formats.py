f97b71173be15fa8733c1567e0b04bc770d8d996
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-06-18 15:41:03 -0400

    fetch next page and next result page

--- formats.py
+++ formats.py
@@ -5,18 +5,11 @@ from bs4 import BeautifulSoup
 clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/study/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
 parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
 
-clinicResultPage = requests.get("https://clinicaltrials.gov/ct2/show/results/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
-parseClinicResultPage = BeautifulSoup(clinicResultPage.content, "html.parser")
-
-currentStudyPage = parseClinicPage.find("div", id="main-content").find("div", id="tabs").find("li", id="tabular").find("a",href=True)
-storeCurrentStudyPage = "https://clinicaltrials.gov" + str(currentStudyPage['href'])
-print(storeCurrentStudyPage)
-
 nextPage = parseClinicPage.find("div", class_="tr-results-nav").find(class_="tr-next-link", href=True)
-storeNextPage = "https://clinicaltrials.gov" + str(nextPage['href'])
+resultPage = str(nextPage['href'])
+slicedResultPage = resultPage[15:]
+storeNextPage = "https://clinicaltrials.gov" + resultPage
+storeNextResultPage = "https://clinicaltrials.gov/ct2/show/results" + slicedResultPage
 print(storeNextPage)
-
-nextResultPage = parseClinicResultPage.find("div", id="main-content").find("div", id="tabs").find("li", id="tabular").find("a",href=True)
-storeNextResultPage = "https://clinicaltrials.gov" + str(nextResultPage['href'])
 print(storeNextResultPage)
 
