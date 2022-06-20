da320977a0822a91c8e970ef09e5f6ee03121128
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-06-20 13:46:38 -0400

    updated format code

--- formats.py
+++ formats.py
@@ -23,11 +23,11 @@ def findFormat():
 
         #get length of row and columns
         #columns
-        colns = pageSoup.find("div", class_="tr-indent1 tr-squishScroll").find_all("table", class_="de-lightBorder")[1].find("tbody").find("tr").find_all()
+        colns = pageSoup.find("div",id="tab-body").find("div", class_="tr-indent2").find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("table", class_="de-lightBorder").find("tbody").find("tr").find_all()
         getColumnFormat = str(len(colns))
 
         #rows
-        rows = pageSoup.find("div", class_="tr-indent1 tr-squishScroll").find_all("table", class_="de-lightBorder")[1].find("tbody").find_all()
+        rows = pageSoup.find("div",id="tab-body").find("div", class_="tr-indent2").find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("table", class_="de-lightBorder").find("tbody").find_all()
         getRowFormat = str(len(rows))
 
         print("The format for page " + str(page) + " is" + " " + getRowFormat + " rows by " + getColumnFormat + " columns")
@@ -35,14 +35,14 @@ def findFormat():
 
 #findFormat()
 
-clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/results/NCT05110014?rslt=With&cntry=US&draw=2&rank=3")
+clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/results/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
 parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
 
-colns = parseClinicPage.find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("div", class_="tr-indent2").find("table", class_="de-lightBorder").find("tbody").find("tr").find_all()
+colns = parseClinicPage.find("div",id="tab-body").find("div", class_="tr-indent2").find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("table", class_="de-lightBorder").find("tbody").find("tr").find_all()
 print(colns)
 getColumnFormat = str(len(colns))
 
-rows = parseClinicPage.find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("div", class_="tr-indent2").find("table", class_="de-lightBorder").find("tbody").find_all()
+rows = parseClinicPage.find("div",id="tab-body").find("div", class_="tr-indent2").find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("table", class_="de-lightBorder").find("tbody").find_all()
 print(rows)
 getRowFormat = str(len(rows))
 
