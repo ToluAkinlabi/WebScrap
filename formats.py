9a4b2194c3fdc90ceb6f3c11c560913d990cfbf9
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-06-22 14:30:15 -0400

    Updated formmat file

--- formats.py
+++ formats.py
@@ -7,7 +7,7 @@ def findFormat():
     format_set = set()
     column_set = set()
     #Looping through the pages
-    pages = np.arange(1,6,1)
+    pages = np.arange(1,38029,1)
     for page in pages:
         clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/study/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
         parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
@@ -33,15 +33,18 @@ def findFormat():
         rows = pageSoup.find("div",id="tab-body").find("div", class_="tr-indent2").find_all("div", class_="tr-indent1 tr-squishScroll")[1].find("table", class_="de-lightBorder").find("tbody").find("tr").find_next_siblings()
         getRowFormat = str(len(rows) + 1)
 
-        #print("The format for page " + str(page) + " is" + " " + getRowFormat + " rows by " + getColumnFormat + " columns")
+        print("The format for page " + str(page) + " is" + " " + getRowFormat + " rows by " + getColumnFormat + " columns")
 
         #stores column format and row format
         format_set.add(getRowFormat + "," + getColumnFormat)
         column_set.add(getColumnFormat)
 
-    #gets the numbers of formats
-    print(f'Length of format: {len(format_set)}')
-    print(f'Format types : {format_set}')
-    print(f'Columns : {column_set}')
+    try :
+        #gets the numbers of formats
+        print(f'Length of format: {len(format_set)}')
+        print(f'Format types : {format_set}')
+        print(f'Columns : {column_set}')
+    except :
+        print("Error found!")
 
 findFormat()
