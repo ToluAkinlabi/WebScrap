b7cadd45a15c9f9486e3c14fe53e5a738ba72f0c
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-07-05 14:34:32 -0400

    updated format code

--- formats.py
+++ formats.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 
 import pandas as pd
 import requests
@@ -8,27 +9,39 @@ import numpy as np
 def findFormat():
     format_set = set()
     column_set = set()
+    lastPage = None
     failedUrl = []
+
     #Looping through the pages
-    pages = np.arange(1,38140,1)
+    pages = np.arange(38169,38172,1)
     for page in pages:
-        while True:
+        while not lastPage:
             try:
-                clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/study/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
-                parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
+
+                #
+                #clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/study/NCT05167370?rslt=With&cntry=US&draw=2&rank=1")
+                #parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
 
                 pageGetter = requests.get("https://clinicaltrials.gov/ct2/show/results?rslt=With&cntry=US&draw=2&rank=" + str(page))
 
                 pageSoup = BeautifulSoup(pageGetter.content, 'html.parser')
-                nextPage = pageSoup.find("div", class_="tr-results-nav").find(class_="tr-next-link", href=True)
-                resultPage = str(nextPage['href'])
 
-                # slice the result page to allow edit to link to display result page
-                slicedResultPage = resultPage[17:]
-                storeNextPage = "https://clinicaltrials.gov/ct2/show/study/" + slicedResultPage  #this gives the full link to the next result page
-                storeNextResultPage = "https://clinicaltrials.gov" + resultPage  # this gives the full link to the next result page
-                #print(storeNextPage)
-                #print(storeNextResultPage)
+                #Test for last page if it exists
+                try:
+                    nextPage = pageSoup.find("div", class_="tr-results-nav").find(class_="tr-next-link", href=True)
+                    resultPage = str(nextPage['href'])
+
+                    # slice the result page to allow edit to link to display result page
+                    slicedResultPage = resultPage[17:]
+                    storeNextPage = "https://clinicaltrials.gov/ct2/show/study/" + slicedResultPage  # this gives the full link to the next result page
+                    storeNextResultPage = "https://clinicaltrials.gov" + resultPage  # this gives the full link to the next result page
+                    # print(storeNextPage)
+                    # print(storeNextResultPage)
+                except:
+                    try:
+                        lastPage = pageSoup.find("div", class_="tr-results-nav").find("span", class_="grayed_fore_color", string="Next Study")
+                    except:
+                        continue
 
                 #get length of row and columns
                 #columns
@@ -44,11 +57,16 @@ def findFormat():
                 #stores column format and row format
                 format_set.add(getRowFormat + "," + getColumnFormat)
                 column_set.add(getColumnFormat)
+
                 break
             except :
                 print("Error found!", sys.exc_info())
                 print(pageGetter)
                 print(page)
+                #traceback.print_exc()
+
+        if lastPage:
+            break
 
     # gets the numbers of formats
     print(f'Length of format: {len(format_set)}')
