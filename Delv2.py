f966cb04bb9d37e3d82e7a419587857e87a4da01
Akinlabi Tolulope <35533505+ToluAkinlabi@users.noreply.github.com>
2022-07-20 17:04:22 -0400

    Save data to json file

--- Delv2.py
+++ Delv2.py
@@ -1,10 +1,18 @@
+import sys
+import traceback
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from pandas import DataFrame
+import numpy as np
+
+df = pd.DataFrame()
 
 # downloads html page and parse page for readability.
-clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/results/NCT04993339?rslt=With&cntry=US&draw=20000&rank=7")
+clinicPage = requests.get("https://clinicaltrials.gov/ct2/show/results/NCT05059366?rslt=With&cntry=US&draw=2&rank=7")
+studyPage = requests.get("https://clinicaltrials.gov/ct2/show/study/NCT05059366?rslt=With&cntry=US&draw=2&rank=7")
 parseClinicPage = BeautifulSoup(clinicPage.content, "html.parser")
+parseStudyPage = BeautifulSoup(studyPage.content, "html.parser")
 
 #Baseline Table Scrap
 #Table header
@@ -26,7 +34,7 @@ total = standardSurgeryWithOOC.find_next()
 strippedTotal = total.text
 
 #Table Title Content
-tableTitleContent = parseClinicPage.find("span", class_="COLLAPSE de-showAndHide").find("span")
+tableTitleContent = parseClinicPage.find("tr", id="EXPAND-armGroupDescriptionRow-baseline").find("span", class_="COLLAPSE de-showAndHide").find("span")
 strippedTitleContent = tableTitleContent.text.strip()
 
 standardSurgeryContent = tableTitleContent.find_next()
@@ -35,8 +43,9 @@ strippedStandardSurgeryContent = standardSurgeryContent.text
 standardSurgeryWithOOContent = standardSurgeryContent.find_next()
 strippedStandardSurgeryWithOOContent = standardSurgeryWithOOContent.text
 
-totalContent = parseClinicPage.find("tr", class_="EXPAND", id="EXPAND-armGroupDescriptionRow-baseline").find("p").find_next().find_next()
+totalContent = standardSurgeryWithOOContent.find_next()
 strippedTotalContent = totalContent.text
+
 
 #Overall Participant
 overallParticipants = parseClinicPage.find("th", class_="de-baselineLabelCell", colspan="2")
@@ -303,87 +312,103 @@ strippedRaceColThree = raceColThree.text
 raceColFour = raceColThree.find_next()
 strippedRaceColFour = raceColFour.text
 
-#Convert scraped data to csv file
-tableCSV = {
+#location
+locCountry = parseStudyPage.find("div", id="COLLAPSE-Locations").find("div", class_="tr-table_cover").find("td", class_="ct-header3")
+strippedLocationCountry = locCountry.text
 
-    # Standard Surgery Section
-    "Standard Surgery" : ["",],
-    "(S) Arm/Group Description" : [strippedStandardSurgeryContent,],
-    "(S) Overall Number of Baseline Participants" : [strippedOverallValueOne,],
-    "(S) Baseline Analysis Population Description" : [strippedAnalysisDescValue,],
-    "(S) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants " : ["",],
-    "(S) Number Analyzed" : [strippedAgeRowOneValueTwo,],
-    "(S) <=18 years" : [strippedAgeRowTwoValueTwo,],
-    " " : [strippedAgeRowTwoValueThree,],
-    "(S) Between 18 and 65 years" : [strippedAgeRowThreeValueTwo,],
-    "  " : [strippedAgeRowThreeValueThree,],
-    "(S) >=65 years" : [strippedAgeRowFourValueTwo,],
-    "   " : [strippedAgeRowFourValueThree,],
-    "(S) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants " : ["",],
-    "(SSex) Number Analyzed" : [strippedSexRowOneValueTwo,],
-    "(S) Female" : [strippedSexRowTwoValueTwo,],
-    "    " :[strippedSexRowTwoValueThree,],
-    "(S) Male" : [strippedSexRowThreeValueTwo,],
-    "     " : [strippedSexRowThreeValueThree,],
-    "(S) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants" : ["",],
-    "(SRace) Number Analyzed" : [strippedRaceRowOneValueTwo,],
-    "(S) Empty" : [strippedRaceRowTwoValueTwo,],
+locDetails = parseStudyPage.find("div", id="COLLAPSE-Locations").find("div", class_="tr-table_cover").find("td", headers="locName")
+strippedLocationDetails = locDetails.text
 
-    #Column Break
-    "         " : [""],
+locCode = parseStudyPage.find("div", id="COLLAPSE-Locations").find("div", class_="tr-table_cover").find_all("td", colspan="2")[1]
+strippedLocationCode = locCode.text
 
-    #Standard Surgery With OOC Section
-    "(SS) Standard Surgery With OOC" : ["",],
-    "(SS) Arm/Group Description" : [strippedStandardSurgeryWithOOContent,],
-    "(SS) Overall Number of Baseline Participants" : [strippedOverallValueTwo,],
-    "(SS) Baseline Analysis Population Description" : [strippedAnalysisDescValue,],
-    "(SS) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants " : ["",],
-    "(SS) Number Analyzed" : [strippedAgeRowOneValueThree,],
-    "(SS) <=18 years" : [strippedAgeRowTwoValueFour,],
-    "           " : [strippedAgeRowTwoValueFive,],
-    "(SS) Between 18 and 65 years" : [strippedAgeRowThreeValueFour,],
-    "            " : [strippedAgeRowThreeValueFive,],
-    "(SS) >=65 years" : [strippedAgeRowFourValueFour,],
-    "              " : [strippedAgeRowFourValueFive,],
-    "(SS) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants " : ["",],
-    "(SSSex) Number Analyzed" : [strippedSexRowOneValueThree,],
-    "(SS) Female" : [strippedSexRowTwoValueFour,],
-    "             " :[strippedSexRowTwoValueFive,],
-    "(SS) Male" : [strippedSexRowThreeValueFour,],
-    "               " : [strippedSexRowThreeValueFive,],
-    "(SS) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants" : ["",],
-    "(SSRace) Number Analyzed" : [strippedRaceRowOneValueThree,],
-    "(SS) Empty" : [strippedRaceRowTwoValueThree,],
 
-    # Column Break
-    "      ": [""],
+#Return result into Json
+data = {
 
-    # Total Section
-    "(T) Total": ["", ],
-    "(T) Arm/Group Description": [strippedTotalContent, ],
-    "(T) Overall Number of Baseline Participants": [strippedOverallValueThree, ],
-    "(T) Baseline Analysis Population Description": [strippedAnalysisDescValue, ],
-    "(T) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants ": ["", ],
-    "(TA) Number Analyzed": [strippedAgeRowOneValueFour, ],
-    "(T) <=18 years": [strippedAgeRowTwoValueSix, ],
-    "       ": [strippedAgeRowTwoValueSeven, ],
-    "(T) Between 18 and 65 years": [strippedAgeRowThreeValueSix, ],
-    "        ": [strippedAgeRowThreeValueSeven, ],
-    "(T) >=65 years": [strippedAgeRowFourValueSix, ],
-    "                 ": [strippedAgeRowFourValueSeven, ],
-    "(T) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants ": ["", ],
-    "(TS) Number Analyzed": [strippedSexRowOneValueFour, ],
-    "(T) Female": [strippedSexRowTwoValueSix, ],
-    "          ": [strippedSexRowTwoValueSeven, ],
-    "(T) Male": [strippedSexRowThreeValueSix, ],
-    "                ": [strippedSexRowThreeValueSeven, ],
-    "(TR) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants": ["", ],
-    "(T) Number Analyzed": [strippedRaceRowOneValueFour, ],
-    "(T) Empty": [strippedRaceRowTwoValueFour, ],
-}
+    # Column 1
 
-dataFrame = pd.DataFrame(tableCSV)
-#print("Converting data to csv file...")
-#print(dataFrame)
-dataFrame.to_csv("C:\\Users\\tolul\\OneDrive\\Documents\\GitHub\\scrap2.csv")
-#print("Done!")
+    '(S) Arm/Group Title': [strippedStandardSurgery],
+    '(S) Arm/Group Description': [strippedStandardSurgeryContent],
+    '(S) Overall Participants': [strippedOverallValueOne],
+    '(S) Baseline Analysis Population Description': [strippedAnalysisDescValue],
+    '(S) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedAgeRowOneValueTwo],
+         '<=18 years': [[strippedAgeRowTwoValueTwo], [strippedAgeRowTwoValueThree]],
+         'Between 18 and 65 years': [[strippedAgeRowThreeValueTwo], [strippedAgeRowThreeValueThree]],
+         '>=65 years': [[strippedAgeRowFourValueTwo], [strippedAgeRowFourValueThree]]
+         }],
+    '(S) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedSexRowOneValueTwo],
+         'Female': [[strippedSexRowTwoValueTwo], [strippedSexRowTwoValueThree]],
+         'Male': [[strippedSexRowThreeValueTwo], [strippedSexRowThreeValueThree]],
+         }],
+    '(S) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants': [{'Number Analyzed': [strippedRaceRowOneValueThree],
+         'Empty': [strippedRaceRowTwoValueTwo]
+         }],
+
+    # Column 2
+
+    '(SS) Arm/Group Title': [strippedStandardSurgeryWithOOC],
+    '(SS) Arm/Group Description': [strippedStandardSurgeryWithOOContent],
+    '(SS) Overall Participants': [strippedOverallValueTwo],
+    '(SS) Baseline Analysis Population Description': [strippedAnalysisDescValue],
+    '(SS) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedAgeRowOneValueThree],
+         '<=18 years': [[strippedAgeRowTwoValueFour], [strippedAgeRowTwoValueFive]],
+         'Between 18 and 65 years': [[strippedAgeRowThreeValueFour], [strippedAgeRowThreeValueFive]],
+         '>=65 years': [[strippedAgeRowFourValueFour], [strippedAgeRowFourValueFive]]
+         }],
+    '(SS) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedSexRowOneValueThree],
+         'Female': [[strippedSexRowTwoValueFour], [strippedSexRowTwoValueFive]],
+         'Male': [[strippedSexRowThreeValueFour], [strippedSexRowThreeValueFive]],
+         }],
+    '(SS) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedRaceRowOneValueThree],
+         'Empty': [strippedRaceRowTwoValueThree]
+         }],
+
+    # Column  3
+
+    '(T) Arm/Group Title': [strippedTotal],
+    '(T) Arm/Group Description': [strippedTotalContent],
+    '(T) Overall Participants': [strippedOverallValueThree],
+    '(T) Baseline Analysis Population Description': [strippedAnalysisDescValue],
+    '(T) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedAgeRowOneValueFour],
+         '<=18 years': [[strippedAgeRowTwoValueSix], [strippedAgeRowTwoValueSeven]],
+         'Between 18 and 65 years': [[strippedAgeRowThreeValueSix], [strippedAgeRowThreeValueSeven]],
+         '>=65 years': [[strippedAgeRowFourValueSix], [strippedAgeRowFourValueSeven]]
+         }],
+    '(T) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedSexRowOneValueFour],
+         'Female': [[strippedSexRowTwoValueSix], [strippedSexRowTwoValueSeven]],
+         'Male': [[strippedSexRowThreeValueSix], [strippedSexRowThreeValueSeven]],
+         }],
+    '(T) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants': [
+        {'Number Analyzed': [strippedSexRowThreeValueSeven],
+         'Empty': [strippedRaceRowTwoValueFour]
+         }]
+
+    }
+
+
+df = df.append(DataFrame(data, columns = ['(S) Arm/Group Title', '(S) Arm/Group Description', '(S) Overall Participants','(S) Baseline Analysis Population Description',
+                                        '(S) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(S) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(S) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants',
+                                          
+                                        '(SS) Arm/Group Title', '(SS) Arm/Group Description', '(SS) Overall Participants','(SS) Baseline Analysis Population Description',
+                                        '(SS) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(SS) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(SS) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants',
+
+                                        '(T) Arm/Group Title', '(T) Arm/Group Description', '(T) Overall Participants','(T) Baseline Analysis Population Description',
+                                        '(T) Age, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(T) Sex, Categorical Measure Type: Count of Participants Unit of measure:  Participants',
+                                        '(T) Race and Ethnicity Not Collected Measure Type: Count of Participants Unit of measure:  Participants',
+                                        ]
+                         ))
+
+df.to_json(r'C:\Users\tolul\OneDrive\Documents\GitHub\WebScrap\delv2.json', orient='records')
